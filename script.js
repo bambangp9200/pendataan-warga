5aa04f12520d4d3d019a9ca8d50357956a747df5
bambangp9200 <bambangpri83@gmail.com>
2025-04-25 14:03:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-body {
-    font-family: sans-serif;
-    padding: 20px;
-    background: #f2f2f2;
-}
-form input {
-    margin: 5px;
-    padding: 8px;
-    width: 100%;
-    max-width: 300px;
-}
-form button {
-    padding: 8px;
-    margin-top: 10px;
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-    cursor: pointer;
-}
-form button:hover {
-    background-color: #45a049;
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC9mRkENyFalVkaTCaNNmnnzpiSz5Yb9WQ",
+  authDomain: "pendataan-warga-4bbaa.firebaseapp.com",
+  projectId: "pendataan-warga-4bbaa",
+  storageBucket: "pendataan-warga-4bbaa.firebasestorage.app",
+  messagingSenderId: "765479321558",
+  appId: "1:765479321558:web:5c88d50a3976b1f5426e77",
+  measurementId: "G-1FHJC7YPHM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
